d3c4f114f965f57ff692800ab6a74f3b3b16eb6f
Emirhan Gueler <emirhangueler52@gmail.com>
2021-12-01 23:26:30 +0100

    Put env variables into environment yaml key

--- api/src/services/redis.ts
+++ api/src/services/redis.ts
@@ -1,6 +1,6 @@
 import ioredis from 'ioredis';
 import { KEYDB_HOST, KEYDB_PASSWORD, KEYDB_PORT } from '../env';
-
+console.log({ KEYDB_HOST, KEYDB_PASSWORD, KEYDB_PORT });
 export const redis = new ioredis(KEYDB_PORT, KEYDB_HOST, {
   password: KEYDB_PASSWORD,
   lazyConnect: true,
